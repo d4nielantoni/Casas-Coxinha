21170439a26861c6b19356d5e0a5b96a73000bb8
d4nielantoni <danielantoni7575@gmail.com>
2024-04-13 19:03:08 -0300

    adicionei outra imagem no slider e melhorei a transição

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const slides = document.querySelectorAll('.slider li');
+    const descriptions = document.querySelectorAll('.slider .slide-description');
+
     let currentSlide = 0;
 
-    function nextSlide() {
-        slides[currentSlide].querySelector('input').checked = false; // Desmarca o input atual
-        currentSlide = (currentSlide + 1) % slides.length; // Avança para o próximo slide
-        slides[currentSlide].querySelector('input').checked = true; // Marca o input do próximo slide
+    function showSlide(index) {
+        slides[currentSlide].querySelector('input').checked = false;
+        descriptions[currentSlide].classList.remove('active');
+        currentSlide = (index + slides.length) % slides.length;
+        slides[currentSlide].querySelector('input').checked = true;
+        descriptions.forEach(description => description.classList.remove('active')); // Remove a classe 'active' de todas as descrições
+        descriptions[currentSlide].classList.add('active'); // Adiciona a classe 'active' apenas à descrição correspondente ao slide atual
     }
 
-    setInterval(nextSlide, 5000); // Muda de slide a cada 5 segundos
+    function nextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    setInterval(nextSlide, 5000);
 });
